b6df4a13d01eeb24b0481c5dc17c5f182e14fe43
Skyler Trent <skyler.trent93@gmail.com>
2022-10-13 22:05:09 -0600

    randomName

---
+++ index.js
@@ -0,0 +1,17 @@
+const names = ["Jessica", "Austin", "Brooke", "Tyler", "Janae", "Josh"];
+
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
+
+const getRandomName = () => {
+  `${names[getRandomNumber(names.length)]}`;
+};
+
+console.log(getRandomName());
+
+// const setRandomName = () => {
+//   document.getElementById("random-name").innerText = getRandomName();
+// };
+
+// document.getElementById("generate").addEventListener("click", setRandomName);
+
+// setRandomName();
